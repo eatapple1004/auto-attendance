f424915e6d279de737125b1ed3e1d47ca0525d48
eatapple1004 <frank.virus0325@gmail.com>
2025-05-12 17:34:57 +0900

    csv파일에사 VO 형태 첫 번환 목적의 VO

---
+++ models/LocationVO.js
@@ -0,0 +1,22 @@
+class LocationVO {
+    constructor(nameAndTypeMap = new Map(), location = '') {
+      this.nameAndTypeMap = nameAndTypeMap; // Map<string, string>
+      this.location = location;             // string 또는 객체
+    }
+  
+    // 선택: name/type 추가 헬퍼 메서드
+    addNameType(name, type) {
+      this.nameAndTypeMap.set(name, type);
+    }
+  
+    // 선택: 전체 name 리스트 반환
+    getNames() {
+      return Array.from(this.nameAndTypeMap.keys());
+    }
+  
+    // 선택: 특정 name의 type 반환
+    getTypeByName(name) {
+      return this.nameAndTypeMap.get(name);
+    }
+  }
+  
