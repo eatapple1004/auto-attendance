98cec129dc59c0fdce8c059b40e25b3a78b3784c
eatapple1004 <frank.virus0325@gmail.com>
2025-05-12 12:49:31 +0900

    add Def.js file

---
+++ Def.js
@@ -0,0 +1,8 @@
+global.DEF_USING_HTTP=true;   // http rest api 사용
+global.DEF_USING_HTTPS=false; // https rest api 사용
+
+global.DEF_APP_TITLE       = "KAKAO SABOK 근태 BE Server";
+global.DEF_APP_CREATE_DATE = "2025.5.12";
+
+global.DEF_EVP_LOCAL_IP        = "not decided yet";
+global.DEF_EVP_LOCAL_HTTP_PORT = 3000;
