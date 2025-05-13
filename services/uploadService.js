bb1aeb6ec40d132f6c51dd320f65e894a50e0a11
eatapple1004 <frank.virus0325@gmail.com>
2025-05-13 19:33:11 +0900

    add parsing data function

--- services/uploadService.js
+++ services/uploadService.js
@@ -15,6 +15,17 @@ exports.processUpload = ({ year, month, file }) => {
     console.log('📁 파일 경로:', file.path);
 };
 
+function trimBeforeDateLine(text) {
+    const lines = text.split('\n');
+    const dateRegex = /^\s*\d{4}\.\d{2}\.\d{2}.*$/;
+  
+    const dateLineIndex = lines.findIndex(line => dateRegex.test(line));
+  
+    if (dateLineIndex === -1) return []; // 날짜 줄이 없으면 빈 배열 반환
+  
+    return lines.slice(dateLineIndex); // 날짜 줄부터 끝까지 반환
+}
+
 function parseCsvFile(filePath) {
     return new Promise((resolve, reject) => {
         const results = [];
@@ -25,6 +36,8 @@ function parseCsvFile(filePath) {
             // row = { name: '홍길동', type: 'admin' } 등
             const columnA = Object.values(row)[0]; // 날짜
             const columnC = Object.values(row)[2]; // 출결 내용
+
+            if (!columnA || !columnC || !trimBeforeDateLine(columnC)) return;
 
             results.push(row);
         })
